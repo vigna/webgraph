c650f8acf11d56d6ef81d25088f1b2fcfad53dd4
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2021-04-06 22:54:09 +0100

    Added build instructions

--- README.md
+++ README.md
@@ -9,4 +9,10 @@ This version of WebGraph is limited to graphs with at most 2^31 nodes. For
 larger graphs, have a look at the [big
 version](https://github.com/vigna/webgraph-big).
 
+Building
+--------
+
+You need [Ant](https://ant.apache.org/) and [Ivy](https://ant.apache.org/ivy/).
+Then, run `ant ivy-setupjars jar`.
+
 seba (<mailto:sebastiano.vigna@unimi.it>)
--- src/it/unimi/dsi/webgraph/EFGraph.java
+++ src/it/unimi/dsi/webgraph/EFGraph.java
@@ -463,9 +463,9 @@ public class EFGraph extends ImmutableGraph {
 			/* Very approximately, half of the cache for lower, half for upper, and a small fraction
 			 * (8/quantum) for pointers. This will generate a much larger cache than expected if
 			 * quantum is very small. */
-			successors = new LongWordCache(Math.max(MIN_CACHE_SIZE, bufferSize >>> Math.max(3, log2Quantum - 3)), "pointers");
-			lowerBits = new LongWordCache(Math.max(MIN_CACHE_SIZE, bufferSize / 2), "lower");
-			upperBits = new LongWordCache(Math.max(MIN_CACHE_SIZE, bufferSize / 2), "upper");
+			successors = new LongWordCache(Math.max(MIN_CACHE_SIZE, bufferSize >>> Math.max(3, log2Quantum - 3)), ".pointers");
+			lowerBits = new LongWordCache(Math.max(MIN_CACHE_SIZE, bufferSize / 2), ".lower");
+			upperBits = new LongWordCache(Math.max(MIN_CACHE_SIZE, bufferSize / 2), ".upper");
 		}
 
 		public int lowerBits() {
