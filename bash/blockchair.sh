32f759f4a8bcc3da13fa505fceb0be76445873d8
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-02-22 14:07:56 +0100

    Added fix in docs

--- bash/blockchair.sh
+++ bash/blockchair.sh
@@ -4,6 +4,7 @@ if [[ "$2" == "" ]]; then
 	echo "$(basename $0) DIR NTHREADS [OUTPUT]" 1>&2
 	echo "Reads files in DIR and processes them using NTHREADS parallel sorts." 1>&2
 	echo "Files are processed as input files unless OUTPUT is specified." 1>&2
+	echo "FILES MUST END WITH A NEWLINE. Fix them with \"sed -i -e '\$a\\' *\"." 1>&2
 	exit 1
 fi
 
@@ -27,10 +28,10 @@ SPLITS=$(for file in ${SPLIT}?*; do echo $file; done)
 
 for file in $SPLITS; do 
 	mkfifo $file.pipe
-	if [[ "$OUTPUT" != "" ]] ;
-		(cut -f2,7,10 $(cat $file) | awk '{ if ($3 != 0) print $2 "\t" $1 }' | sort -k2 -S2G >$file.pipe) &
+	if [[ "$OUTPUT" != "" ]]; then
+		(tail -q -n+2 $(cat $file) | cut -f2,7,10 | awk '{ if ($3 != 0) print $2 "\t" $1 }' | sort -k2 -S2G >$file.pipe) &
 	else
-		(cut -f7,13 $(cat $file) | sort -k2 -S2G >$file.pipe) &
+		(tail -q -n+2 $(cat $file) | cut -f7,13 | sort -k2 -S2G >$file.pipe) &
 	fi
 done
 
