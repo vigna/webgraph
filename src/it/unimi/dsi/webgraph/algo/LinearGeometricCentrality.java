3bfe18071ea0c3ef58ce2e23fea78532c74ec499
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2025-06-22 10:39:44 +0200

    Bumped revision

--- src/it/unimi/dsi/webgraph/algo/LinearGeometricCentrality.java
+++ src/it/unimi/dsi/webgraph/algo/LinearGeometricCentrality.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2023 Sebastiano Vigna
+ * Copyright (C) 2013-2025 Paolo Boldi
  *
  * This program and the accompanying materials are made available under the
  * terms of the GNU Lesser General Public License v2.1 or later,
@@ -49,28 +49,34 @@ import it.unimi.dsi.webgraph.ArrayListMutableGraph;
 import it.unimi.dsi.webgraph.ImmutableGraph;
 import it.unimi.dsi.webgraph.LazyIntIterator;
 
-/** Computes exactly a <em>positive</em> linear geometric centrality with specified coefficient vector
- * and the number of reachable nodes using multiple parallel breadth-first visits.
- * A survey about linear geometric centralities can be found in: Boldi, Paolo, Flavio Furia, and 
- * Chiara Prezioso. <em>Linear Geometric Centralities</em>. International Workshop on Modelling and Mining Networks. Springer, Cham, 2025.
- * 
- * 
- * <p>Note that usually one is interested in the <em>negative</em> version of a centrality measure, that is, the version
- * that depends on the <em>incoming</em> arcs. This class can compute only <em>positive</em> centralities: if you are
- * interested (as it usually happens) in the negative version, you must pass to this class the <em>transpose</em> of the graph.
+/**
+ * Computes exactly a <em>positive</em> linear geometric centrality with specified coefficient
+ * vector and the number of reachable nodes using multiple parallel breadth-first visits. A survey
+ * about linear geometric centralities can be found in: Boldi, Paolo, Flavio Furia, and Chiara
+ * Prezioso. <em>Linear Geometric Centralities</em>. International Workshop on Modelling and Mining
+ * Networks. Springer, Cham, 2025.
  *
- * <p>Every visit is independent and is carried out by a separate thread. The downside is
- * that running on <var>k</var> cores requires approximately <var>k</var> times the memory of the
- * sequential algorithm, as only the graph and the betweenness array will be shared.
  *
- * <p>To use this class you first create an instance, and then invoke {@link #compute()}.
- * After that, you can peek at the fields {@link #centrality} and {@link #reachable}.
+ * <p>
+ * Note that usually one is interested in the <em>negative</em> version of a centrality measure,
+ * that is, the version that depends on the <em>incoming</em> arcs. This class can compute only
+ * <em>positive</em> centralities: if you are interested (as it usually happens) in the negative
+ * version, you must pass to this class the <em>transpose</em> of the graph.
+ *
+ * <p>
+ * Every visit is independent and is carried out by a separate thread. The downside is that running
+ * on <var>k</var> cores requires approximately <var>k</var> times the memory of the sequential
+ * algorithm, as only the graph and the betweenness array will be shared.
+ *
+ * <p>
+ * To use this class you first create an instance, and then invoke {@link #compute()}. After that,
+ * you can peek at the fields {@link #centrality} and {@link #reachable}.
  */
 
 public class LinearGeometricCentrality {
 	private final static Logger LOGGER = LoggerFactory.getLogger(LinearGeometricCentrality.class);
 
-	/** Corresponds to the sequence of coefficients [0, 1/1, 1/2,  1/3, &hellip;] */
+	/** Corresponds to the sequence of coefficients [0, 1/1, 1/2, 1/3, &hellip;] */
 	final static public class HarmonicCoefficients implements Int2DoubleFunction {
 		@Override
 		public double get(int distance) {
@@ -81,28 +87,34 @@ public class LinearGeometricCentrality {
 		}
 	}
 
-	/** Given an exponent &gamma; corresponds to the sequence of coefficients [0<sup>&gamma;</sup>, 1<sup>&gamma;</sup>, 2<sup>&gamma;</sup>, 3<sup>&gamma;</sup>, &hellip;] */
+	/**
+	 * Given an exponent &gamma; corresponds to the sequence of coefficients [0<sup>&gamma;</sup>,
+	 * 1<sup>&gamma;</sup>, 2<sup>&gamma;</sup>, 3<sup>&gamma;</sup>, &hellip;]
+	 */
 	final static public class PowerLawCoefficients implements Int2DoubleFunction {
 		private double exponent;
 
 		public PowerLawCoefficients(final double exponent) {
 			this.exponent = exponent;
 		}
-		
+
 		@Override
 		public double get(int distance) {
 			return Math.pow(distance, exponent);
 		}
 	}
 
-	/** Given a base &beta; corresponds to the sequence of coefficients [&beta;<sup>0</sup>, &beta;<sup>1</sup>, &beta;<sup>2</sup>, &beta;<sup>3</sup>, &hellip;] */
+	/**
+	 * Given a base &beta; corresponds to the sequence of coefficients [&beta;<sup>0</sup>,
+	 * &beta;<sup>1</sup>, &beta;<sup>2</sup>, &beta;<sup>3</sup>, &hellip;]
+	 */
 	final static public class ExponentialCoefficients implements Int2DoubleFunction {
 		private double base;
 
 		public ExponentialCoefficients(final double base) {
 			this.base = base;
 		}
-		
+
 		@Override
 		public double get(int distance) {
 			return Math.pow(base, distance);
@@ -126,10 +138,13 @@ public class LinearGeometricCentrality {
 	/** Whether to stop abruptly the visiting process. */
 	protected volatile boolean stop;
 
-	/** Creates a new class for computing a positive linear geometric centrality and the number reachable nodes.
+	/**
+	 * Creates a new class for computing a positive linear geometric centrality and the number reachable
+	 * nodes.
 	 *
 	 * @param graph a graph.
-	 * @param requestedThreads the requested number of threads (0 for {@link Runtime#availableProcessors()}).
+	 * @param requestedThreads the requested number of threads (0 for
+	 *            {@link Runtime#availableProcessors()}).
 	 * @param pl a progress logger, or {@code null}.
 	 */
 	public LinearGeometricCentrality(final ImmutableGraph graph, final IntToDoubleFunction coeffs, final int requestedThreads, final ProgressLogger pl) {
@@ -142,8 +157,9 @@ public class LinearGeometricCentrality {
 		numberOfThreads = requestedThreads != 0 ? requestedThreads : Runtime.getRuntime().availableProcessors();
 	}
 
-	/** Creates a new class for computing a positive linear geometric centrality and the number of reachable nodes, using as many threads as
-	 *  the number of available processors.
+	/**
+	 * Creates a new class for computing a positive linear geometric centrality and the number of
+	 * reachable nodes, using as many threads as the number of available processors.
 	 *
 	 * @param graph a graph.
 	 * @param pl a progress logger, or {@code null}.
@@ -152,17 +168,21 @@ public class LinearGeometricCentrality {
 		this(graph, coeffs, 0, pl);
 	}
 
-	/** Creates a new class for computing a positive linear geometric centrality and the number of reachable nodes.
+	/**
+	 * Creates a new class for computing a positive linear geometric centrality and the number of
+	 * reachable nodes.
 	 *
 	 * @param graph a graph.
-	 * @param requestedThreads the requested number of threads (0 for {@link Runtime#availableProcessors()}).
+	 * @param requestedThreads the requested number of threads (0 for
+	 *            {@link Runtime#availableProcessors()}).
 	 */
 	public LinearGeometricCentrality(final ImmutableGraph graph, final IntToDoubleFunction coeffs, final int requestedThreads) {
 		this(graph, coeffs, 1, null);
 	}
 
-	/** Creates a new class for computing a positive linear geometric centralities and the number of reachable nodes, using as many threads as
-	 *  the number of available processors.
+	/**
+	 * Creates a new class for computing a positive linear geometric centralities and the number of
+	 * reachable nodes, using as many threads as the number of available processors.
 	 *
 	 * @param graph a graph.
 	 */
@@ -173,7 +193,10 @@ public class LinearGeometricCentrality {
 	private final class IterationThread implements Callable<Void> {
 		/** The queue of visited nodes. */
 		private final IntArrayFIFOQueue queue;
-		/** The array containing the distance of each node from the current source (or -1 if the node has not yet been reached by the visit). */
+		/**
+		 * The array containing the distance of each node from the current source (or -1 if the node has not
+		 * yet been reached by the visit).
+		 */
 		private final int[] distance;
 
 		private IterationThread() {
@@ -189,7 +212,7 @@ public class LinearGeometricCentrality {
 			final ImmutableGraph graph = LinearGeometricCentrality.this.graph.copy();
 			final IntToDoubleFunction coeffs = LinearGeometricCentrality.this.coeffs;
 
-			for(;;) {
+			for (;;) {
 				final int curr = nextNode.getAndIncrement();
 				if (LinearGeometricCentrality.this.stop || curr >= graph.numNodes()) return null;
 				queue.clear();
@@ -198,13 +221,13 @@ public class LinearGeometricCentrality {
 				distance[curr] = 0;
 				int reachable = 0;
 
-				while(! queue.isEmpty()) {
+				while (!queue.isEmpty()) {
 					final int node = queue.dequeueInt();
 					reachable++;
 					final int d = distance[node] + 1;
 					final double coeff = coeffs.applyAsDouble(d);
 					final LazyIntIterator successors = graph.successors(node);
-					for(int s; (s = successors.nextInt()) != -1;) {
+					for (int s; (s = successors.nextInt()) != -1;) {
 						if (distance[s] == -1) {
 							queue.enqueue(s);
 							distance[s] = d;
@@ -213,22 +236,22 @@ public class LinearGeometricCentrality {
 					}
 				}
 
-				if (LinearGeometricCentrality.this.pl != null)
-					synchronized (LinearGeometricCentrality.this.pl) {
-						LinearGeometricCentrality.this.pl.update();
-					}
+				if (LinearGeometricCentrality.this.pl != null) synchronized (LinearGeometricCentrality.this.pl) {
+					LinearGeometricCentrality.this.pl.update();
+				}
 
 				LinearGeometricCentrality.this.reachable[curr] = reachable;
 			}
 		}
 	}
 
-
-	/** Computes a linear geometric centralitu and the number of reachable nodes.
-	 * Results can be found in {@link LinearGeometricCentrality#centrality} and {@link LinearGeometricCentrality#reachable}. */
+	/**
+	 * Computes a linear geometric centralitu and the number of reachable nodes. Results can be found in
+	 * {@link LinearGeometricCentrality#centrality} and {@link LinearGeometricCentrality#reachable}.
+	 */
 	public void compute() throws InterruptedException {
 		final IterationThread[] thread = new IterationThread[numberOfThreads];
-		for(int i = 0; i < thread.length; i++) thread[i] = new IterationThread();
+		for (int i = 0; i < thread.length; i++) thread[i] = new IterationThread();
 
 		if (pl != null) {
 			pl.start("Starting visits...");
@@ -239,39 +262,34 @@ public class LinearGeometricCentrality {
 		final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 		final ExecutorCompletionService<Void> executorCompletionService = new ExecutorCompletionService<>(executorService);
 
-		for(int i = thread.length; i-- != 0;) executorCompletionService.submit(thread[i]);
+		for (int i = thread.length; i-- != 0;) executorCompletionService.submit(thread[i]);
 
 		try {
-			for(int i = thread.length; i-- != 0;) executorCompletionService.take().get();
-		}
-		catch(final ExecutionException e) {
+			for (int i = thread.length; i-- != 0;) executorCompletionService.take().get();
+		} catch (final ExecutionException e) {
 			stop = true;
 			final Throwable cause = e.getCause();
 			throw cause instanceof RuntimeException ? (RuntimeException)cause : new RuntimeException(cause.getMessage(), cause);
-		}
-		finally {
+		} finally {
 			executorService.shutdown();
 			final double zeroContribution = coeffs.applyAsDouble(0);
-			for (int i = 0; i < centrality.length; i++) centrality[i] += zeroContribution;  // Node itself is not considered
+			for (int i = 0; i < centrality.length; i++) centrality[i] += zeroContribution; // Node itself is not
+																							// considered
 		}
 
 		if (pl != null) pl.done();
 	}
 
-
 	public static void main(final String[] arg) throws IOException, JSAPException, InterruptedException, NoSuchMethodException, SecurityException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
-		final SimpleJSAP jsap = new SimpleJSAP(LinearGeometricCentrality.class.getName(), "Computes a positive linear geometric centrality of a graph using multiple parallel breadth-first visits.\n\nPlease note that to compute negative centralities on directed graphs (which is usually what you want) you have to compute positive centralities on the transpose.",
-			new Parameter[] {
-			new Switch("expand", 'e', "expand", "Expand the graph to increase speed (no compression)."),
-			new Switch("mapped", 'm', "mapped", "Use loadMapped() to load the graph."),
-			new FlaggedOption("threads", JSAP.INTSIZE_PARSER, "0", JSAP.NOT_REQUIRED, 'T', "threads", "The number of threads to be used. If 0, the number will be estimated automatically."),
-			new UnflaggedOption("graphBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The basename of the graph."),
-			new UnflaggedOption("coefficientsSpec", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The coefficient class spec: CLASSNAME or CLASSNAME(arg,...)."),
-			new UnflaggedOption("centralityFilename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The filename where centrality scores (doubles in binary form) will be stored."),
-			new UnflaggedOption("reachableFilename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The filename where the number of reachable nodes (longs in binary form) will be stored.")
-		}
-		);
+		final SimpleJSAP jsap = new SimpleJSAP(LinearGeometricCentrality.class.getName(), "Computes a positive linear geometric centrality of a graph using multiple parallel breadth-first visits.\n\nPlease note that to compute negative centralities on directed graphs (which is usually what you want) you have to compute positive centralities on the transpose.", new Parameter[] {
+				new Switch("expand", 'e', "expand", "Expand the graph to increase speed (no compression)."),
+				new Switch("mapped", 'm', "mapped", "Use loadMapped() to load the graph."),
+				new FlaggedOption("threads", JSAP.INTSIZE_PARSER, "0", JSAP.NOT_REQUIRED, 'T', "threads", "The number of threads to be used. If 0, the number will be estimated automatically."),
+				new UnflaggedOption("graphBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The basename of the graph."),
+				new UnflaggedOption("coefficientsSpec", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The coefficient class spec: CLASSNAME or CLASSNAME(arg,...)."),
+				new UnflaggedOption("centralityFilename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The filename where centrality scores (doubles in binary form) will be stored."),
+				new UnflaggedOption("reachableFilename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, JSAP.NOT_GREEDY, "The filename where the number of reachable nodes (longs in binary form) will be stored.") });
 
 		final JSAPResult jsapResult = jsap.parse(arg);
 		if (jsap.messagePrinted()) System.exit(1);
@@ -284,16 +302,16 @@ public class LinearGeometricCentrality {
 			String[] args = coefficientsSpec.substring(opp + 1, coefficientsSpec.length() - 1).split(",");
 			System.out.println(className);
 			Object[] boxedDoubleArgs = java.util.Arrays.stream(args).mapToDouble(Double::parseDouble).boxed().toArray();
-			
+
 			Class<?>[] parameterTypes = new Class<?>[boxedDoubleArgs.length];
 			Arrays.fill(parameterTypes, double.class);
 			Constructor<?> constructor = Class.forName(className).getConstructor(parameterTypes);
-			coeffs = (IntToDoubleFunction) constructor.newInstance(boxedDoubleArgs);
+			coeffs = (IntToDoubleFunction)constructor.newInstance(boxedDoubleArgs);
 		} else {
 			Constructor<?> constructor = Class.forName(coefficientsSpec).getConstructor();
-			coeffs = (IntToDoubleFunction) constructor.newInstance();
+			coeffs = (IntToDoubleFunction)constructor.newInstance();
 		}
-		
+
 		final boolean mapped = jsapResult.getBoolean("mapped", false);
 		final String graphBasename = jsapResult.getString("graphBasename");
 		final int threads = jsapResult.getInt("threads");
@@ -301,10 +319,10 @@ public class LinearGeometricCentrality {
 		progressLogger.displayFreeMemory = true;
 		progressLogger.displayLocalSpeed = true;
 
-		ImmutableGraph graph = mapped? ImmutableGraph.loadMapped(graphBasename, progressLogger) : ImmutableGraph.load(graphBasename, progressLogger);
+		ImmutableGraph graph = mapped ? ImmutableGraph.loadMapped(graphBasename, progressLogger) : ImmutableGraph.load(graphBasename, progressLogger);
 		if (jsapResult.userSpecified("expand")) graph = new ArrayListMutableGraph(graph).immutableView();
 
-		final LinearGeometricCentrality centralities = new LinearGeometricCentrality(graph, coeffs, threads, progressLogger); 
+		final LinearGeometricCentrality centralities = new LinearGeometricCentrality(graph, coeffs, threads, progressLogger);
 		centralities.compute();
 
 		BinIO.storeDoubles(centralities.centrality, jsapResult.getString("centralityFilename"));
--- test/it/unimi/dsi/webgraph/algo/LinearGeometricCentralitiesTest.java
+++ test/it/unimi/dsi/webgraph/algo/LinearGeometricCentralitiesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2023 Paolo Boldi, Massimo Santini and Sebastiano Vigna
+ * Copyright (C) 2011-2025 Paolo Boldi
  *
  * This program and the accompanying materials are made available under the
  * terms of the GNU Lesser General Public License v2.1 or later,
@@ -28,15 +28,14 @@ import it.unimi.dsi.webgraph.ImmutableGraph;
 import it.unimi.dsi.webgraph.Transform;
 import it.unimi.dsi.webgraph.examples.ErdosRenyiGraph;
 
-
-
 //RELEASE-STATUS: DIST
 
 public class LinearGeometricCentralitiesTest {
 
 	@Test
 	public void testPath() throws InterruptedException {
-		final ImmutableGraph graph = Transform.transpose(new ArrayListMutableGraph(3, new int[][] { { 0, 1 }, { 1, 2 } }).immutableView());
+		final ImmutableGraph graph = Transform.transpose(new ArrayListMutableGraph(3, new int[][] { { 0, 1 },
+				{ 1, 2 } }).immutableView());
 
 		final GeometricCentralities centralities = new GeometricCentralities(graph);
 		centralities.compute();
@@ -45,25 +44,24 @@ public class LinearGeometricCentralitiesTest {
 
 		assertEquals(0, centralities.harmonic[0], 0);
 		assertEquals(1, centralities.harmonic[1], 0);
-		assertEquals(3./2, centralities.harmonic[2], 0);
+		assertEquals(3. / 2, centralities.harmonic[2], 0);
 		assertEquals(0, linear.centrality[0], 0);
 		assertEquals(1, linear.centrality[1], 0);
-		assertEquals(3./2, linear.centrality[2], 0);
+		assertEquals(3. / 2, linear.centrality[2], 0);
 	}
 
 	@Test
 	public void testErdosRenyi() throws IOException, InterruptedException {
-		for(final int size: new int[] { 10, 100 }) {
-			for(final double density: new double[] { 0.0001, 0.001, 0.01 }) {
+		for (final int size : new int[] { 10, 100 }) {
+			for (final double density : new double[] { 0.0001, 0.001, 0.01 }) {
 				final ImmutableGraph g = new ArrayListMutableGraph(new ErdosRenyiGraph(size, density, 0, false)).immutableView();
 				final HyperBall hanf = new HyperBall(g, Transform.transpose(g), 20, null, 0, 0, 0, false, true, true, null, 0);
 				hanf.init();
-				do hanf.iterate(); while(hanf.modified() != 0);
+				do hanf.iterate(); while (hanf.modified() != 0);
 				final LinearGeometricCentrality linear = new LinearGeometricCentrality(g, new LinearGeometricCentrality.HarmonicCoefficients());
 				linear.compute();
 
-				for(int i = 0; i < size; i++)
-					assertEquals(hanf.sumOfInverseDistances[i], linear.centrality[i], 1E-3);
+				for (int i = 0; i < size; i++) assertEquals(hanf.sumOfInverseDistances[i], linear.centrality[i], 1E-3);
 				hanf.close();
 			}
 		}
@@ -71,33 +69,27 @@ public class LinearGeometricCentralitiesTest {
 
 	@Test
 	public void testErdosRenyiBis() throws IOException, InterruptedException {
-		for(final int size: new int[] { 10, 100 }) {
-			for(final double density: new double[] { 0.0001, 0.001, 0.01 }) {
+		for (final int size : new int[] { 10, 100 }) {
+			for (final double density : new double[] { 0.0001, 0.001, 0.01 }) {
 				final ImmutableGraph g = new ArrayListMutableGraph(new ErdosRenyiGraph(size, density, 0, false)).immutableView();
 				LinearGeometricCentrality linear = new LinearGeometricCentrality(g, new LinearGeometricCentrality.PowerLawCoefficients(0));
 				linear.compute();
 
-				for(int i = 0; i < size; i++) 
-					assertEquals(linear.reachable[i], linear.centrality[i], 1E-3);
+				for (int i = 0; i < size; i++) assertEquals(linear.reachable[i], linear.centrality[i], 1E-3);
 
 				final GeometricCentralities centralities = new GeometricCentralities(g);
-				centralities.compute(); 
-				linear = new LinearGeometricCentrality(g, x->-x);  // Negative peripherality
+				centralities.compute();
+				linear = new LinearGeometricCentrality(g, x -> -x); // Negative peripherality
 				linear.compute();
-				for(int i = 0; i < size; i++) {
-					if (centralities.closeness[i] != 0)
-						assertEquals(-1.0 / centralities.closeness[i], linear.centrality[i], 1E-3);
-					else
-						assertEquals(centralities.closeness[i], 0, 1E-3);
+				for (int i = 0; i < size; i++) {
+					if (centralities.closeness[i] != 0) assertEquals(-1.0 / centralities.closeness[i], linear.centrality[i], 1E-3);
+					else assertEquals(centralities.closeness[i], 0, 1E-3);
 				}
-				
+
 				linear = new LinearGeometricCentrality(g, new LinearGeometricCentrality.ExponentialCoefficients(1));
 				linear.compute();
 
-				for(int i = 0; i < size; i++) 
-					assertEquals(linear.reachable[i], linear.centrality[i], 1E-3);
-
-
+				for (int i = 0; i < size; i++) assertEquals(linear.reachable[i], linear.centrality[i], 1E-3);
 
 			}
 		}
